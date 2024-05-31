40baa1e97d730a3ec2308e450005a51a78a680b3
Arkan Mahardika <arkan.mahardika.1@gmail.com>
2024-05-31 20:40:28 +0700

    Fix OTP Request & Verify OTP

--- app/src/main/java/com/arkan/terbangin/presentation/auth/otp/OTPViewModel.kt
+++ app/src/main/java/com/arkan/terbangin/presentation/auth/otp/OTPViewModel.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.asLiveData
 import com.arkan.terbangin.data.repository.auth.OTPRepository
+import com.arkan.terbangin.data.repository.auth.RegisterRepository
 import kotlinx.coroutines.Dispatchers
 
 class OTPViewModel(
     extras: Bundle?,
-    private val repository: OTPRepository,
+    private val otpRepository: OTPRepository,
+    private val registerRepository: RegisterRepository,
 ) : ViewModel() {
     val fullName = extras?.getString(OTPActivity.EXTRA_NAME)
     val email = extras?.getString(OTPActivity.EXTRA_EMAIL)
@@ -18,8 +20,25 @@ class OTPViewModel(
     fun verifyOTP(
         email: String,
         otp: String,
-    ) = repository.verifyOTP(
+    ) = otpRepository.verifyOTP(
         email,
         otp,
     ).asLiveData(Dispatchers.IO)
+
+    fun doRegister(
+        fullName: String,
+        email: String,
+        phoneNumber: String,
+        password: String,
+    ) = registerRepository.doRegister(
+        fullName,
+        email,
+        phoneNumber,
+        password,
+    ).asLiveData(Dispatchers.IO)
+
+    fun requestOTP(email: String) =
+        otpRepository.requestOTP(
+            email,
+        ).asLiveData(Dispatchers.IO)
 }
