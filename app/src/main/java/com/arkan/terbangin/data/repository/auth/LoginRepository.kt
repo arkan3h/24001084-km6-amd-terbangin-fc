8b17771023ed9715eef02d4ab924a3b0011222b0
daudhiyaa <daud.dhiya31@gmail.com>
2024-06-07 23:52:26 +0700

    refactor: create utility function to convert String to RequestBody

--- app/src/main/java/com/arkan/terbangin/data/repository/auth/LoginRepository.kt
+++ app/src/main/java/com/arkan/terbangin/data/repository/auth/LoginRepository.kt
@@ -1,14 +1,12 @@
 package com.arkan.terbangin.data.repository.auth
 
-import android.util.Log
 import com.arkan.terbangin.data.datasource.auth.login.LoginDataSource
 import com.arkan.terbangin.data.datasource.preference.PreferenceDataSource
 import com.arkan.terbangin.data.source.network.model.auth.login.LoginResponse
 import com.arkan.terbangin.utils.ResultWrapper
+import com.arkan.terbangin.utils.createPartFromString
 import com.arkan.terbangin.utils.proceedFlow
 import kotlinx.coroutines.flow.Flow
-import okhttp3.MediaType.Companion.toMediaTypeOrNull
-import okhttp3.RequestBody.Companion.toRequestBody
 
 interface LoginRepository {
     fun doLogin(
@@ -25,10 +23,9 @@ class LoginRepositoryImpl(
         email: String,
         password: String,
     ): Flow<ResultWrapper<LoginResponse>> {
-        Log.d("UserRepository", "Sending form with data: email=$email")
         return proceedFlow {
-            val emailBody = email.toRequestBody("text/plain".toMediaTypeOrNull())
-            val passwordBody = password.toRequestBody("text/plain".toMediaTypeOrNull())
+            val emailBody = createPartFromString(email)
+            val passwordBody = createPartFromString(password)
             val loginResponse = dataSource.doLogin(emailBody, passwordBody)
 
             loginResponse.data?.token?.let {
