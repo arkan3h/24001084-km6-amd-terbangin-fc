62ea099b77c13a25c5b7844399c827678d629bfc
Deny Iqbal Mubarok <denyiqbalmubarok@gmail.com>
2024-06-13 22:24:56 +0700

    Finishing Layout Passenger BioData

--- app/src/main/java/com/arkan/terbangin/presentation/checkout/passengerbiodata/PassengerBioDataActivity.kt
+++ app/src/main/java/com/arkan/terbangin/presentation/checkout/passengerbiodata/PassengerBioDataActivity.kt
@@ -3,37 +3,37 @@ package com.arkan.terbangin.presentation.checkout.passengerbiodata
 import android.app.DatePickerDialog
 import android.icu.util.Calendar
 import android.os.Bundle
-import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
-import android.widget.AutoCompleteTextView
-import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isVisible
+import androidx.core.content.ContextCompat
 import com.arkan.terbangin.R
 import com.arkan.terbangin.databinding.ActivityPassengerBiodataBinding
-import com.google.android.material.materialswitch.MaterialSwitch
-import com.google.android.material.textfield.TextInputEditText
+import com.arkan.terbangin.databinding.ItemPassengerBiodataBinding
 
 class PassengerBioDataActivity : AppCompatActivity() {
     private val binding: ActivityPassengerBiodataBinding by lazy {
         ActivityPassengerBiodataBinding.inflate(layoutInflater)
     }
 
+    private val bindingForm: ItemPassengerBiodataBinding by lazy {
+        ItemPassengerBiodataBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        setupLabelTitle()
         setSwitchBioData()
+        setupLabelTitle()
         setClickListener()
     }
 
     private fun setClickListener() {
-        findViewById<View>(R.id.til_date_birth_biodata).setOnClickListener {
+        bindingForm.tilDateBirthBiodata.setOnClickListener {
             showDateBirthPickerDialog()
         }
-        findViewById<View>(R.id.til_date_valid_until).setOnClickListener {
+        bindingForm.tilDateValidUntil.setOnClickListener {
             showDateValidUntilPickerDialog()
         }
     }
@@ -44,15 +44,18 @@ class PassengerBioDataActivity : AppCompatActivity() {
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val dateValidUntilPickerDialog = DatePickerDialog(
-            this,
-            { _, selectedYear, selectedMonth, selectedDay ->
-                // Set the selected date to the input
-                val formattedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
-                findViewById<TextInputEditText>(R.id.ti_et_date_valid_until).setText(formattedDate)
-            },
-            year, month, day
-        )
+        val dateValidUntilPickerDialog =
+            DatePickerDialog(
+                this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    // Set the selected date to the input
+                    val formattedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
+                    bindingForm.tiEtDateValidUntil.setText(formattedDate)
+                },
+                year,
+                month,
+                day,
+            )
 
         dateValidUntilPickerDialog.show()
     }
@@ -63,39 +66,59 @@ class PassengerBioDataActivity : AppCompatActivity() {
         val month = calendar.get(Calendar.MONTH)
         val day = calendar.get(Calendar.DAY_OF_MONTH)
 
-        val dateBirthPickerDialog = DatePickerDialog(
-            this,
-            { _, selectedYear, selectedMonth, selectedDay ->
-                // Set the selected date to the input
-                val formattedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
-                findViewById<TextInputEditText>(R.id.ti_et_date_birth_biodata).setText(formattedDate)
-            },
-            year, month, day
-        )
+        val dateBirthPickerDialog =
+            DatePickerDialog(
+                this,
+                { _, selectedYear, selectedMonth, selectedDay ->
+                    // Set the selected date to the input
+                    val formattedDate = String.format("%02d/%02d/%04d", selectedDay, selectedMonth + 1, selectedYear)
+                    bindingForm.tiEtDateBirthBiodata.setText(formattedDate)
+                },
+                year,
+                month,
+                day,
+            )
 
         dateBirthPickerDialog.show()
     }
 
     private fun setSwitchBioData() {
-        val switch = findViewById<MaterialSwitch>(R.id.switch_ask_name_family)
-        val textNameFamily = findViewById<TextView>(R.id.tv_name_family_biodata)
-        val inputEditTextNameFamily = findViewById<TextInputEditText>(R.id.til_name_family_biodata)
+        val switchAskNameFamily = bindingForm.switchAskNameFamily
+        val tvNameFamilyBiodata = bindingForm.tvNameFamilyBiodata
+        val tilNameFamilyBiodata = bindingForm.tilNameFamilyBiodata
+        val tiEtNameFamilyBiodata = bindingForm.tiEtNameFamilyBiodata
 
-        switch.setOnCheckedChangeListener { _, isChecked ->
-            textNameFamily.isVisible = isChecked
-            inputEditTextNameFamily.isVisible = isChecked
+        switchAskNameFamily.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                tvNameFamilyBiodata.isEnabled = true
+                tilNameFamilyBiodata.isEnabled = true
+                tvNameFamilyBiodata.setTextColor(ContextCompat.getColor(this, R.color.md_theme_primary))
+                tiEtNameFamilyBiodata.setTextColor(ContextCompat.getColor(this, R.color.md_theme_primary))
+                tiEtNameFamilyBiodata.isEnabled = true
+            } else {
+                tvNameFamilyBiodata.isEnabled = false
+                tilNameFamilyBiodata.isEnabled = false
+                tvNameFamilyBiodata.setTextColor(
+                    ContextCompat.getColor(this, R.color.md_theme_outline),
+                )
+                tiEtNameFamilyBiodata.setTextColor(
+                    ContextCompat.getColor(this, R.color.md_theme_outline),
+                )
+                tiEtNameFamilyBiodata.isEnabled = false
+            }
         }
     }
 
     private fun setupLabelTitle() {
         val items = listOf("Mr.", "Mrs.", "Ms.", "Dr.")
-        val autoComplete: AutoCompleteTextView = findViewById(R.id.autoComplete_title)
-        val adapter = ArrayAdapter(this, R.layout.item_passenger_biodata, items)
+        val autoComplete = bindingForm.autoCompleteTitle
+        val adapter = ArrayAdapter(this, R.layout.item_label_title_biodata, items)
 
         autoComplete.setAdapter(adapter)
-        autoComplete.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, i, l ->
-            val itemSelected = adapterView.getItemAtPosition(i)
-            Toast.makeText(this, "Item : $itemSelected", Toast.LENGTH_SHORT).show()
-        }
+        autoComplete.onItemClickListener =
+            AdapterView.OnItemClickListener { adapterView, view, i, l ->
+                val itemSelected = adapterView.getItemAtPosition(i)
+                Toast.makeText(this, "Item : $itemSelected", Toast.LENGTH_SHORT).show()
+            }
     }
 }
